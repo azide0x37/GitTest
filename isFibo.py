d85b9b9394068e624272fcd6b9ec41111f0515cb
Alexander Templeton <blakmatr@gmail.com>
2015-01-22 01:33:55 -0600

    Now functions, added testing abilities; try/except

--- isFibo.py
+++ isFibo.py
@@ -1,7 +1,25 @@
-import * from math
+import math
 
 def isFibo(test):
-	if sqrt(5*test**2 - 4).is_integer() or sqrt(5*test**2 + 4).is_integer()
+	if math.sqrt(5*test**2 - 4).is_integer() or math.sqrt(5*test**2 + 4).is_integer():
 		return True
-	else
+	else:
 		return False
+
+def nextFibo(fibo):
+	fiboLast = 0
+	for n in range(1,fibo):
+		if isFibo(fibo - n):
+			fiboLast = fibo-n
+			return fibo + fiboLast
+
+while raw_input('Continue? ') != 'N\n' or 'n\n':
+	try:
+		test = int(raw_input('Enter a number to find the next Fibonacci number '))
+		if type(test) == int: 
+			if isFibo(test):
+				print test, "is in the fibonacci sequence, and", nextFibo(test), "is it's successor!"
+		else:
+			print test, "Is not a number"
+	except:
+		print "Invalid entry, try again with a number!"
